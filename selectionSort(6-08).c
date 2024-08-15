6235fb9bb9f9ff5e0d743eae2949d3ebe5661c05
KumariUpasna <159221493+KumariUpasna@users.noreply.github.com>
2024-08-15 17:32:29 +0530

    Add files via upload

---
+++ selectionSort(6-08).c
@@ -0,0 +1,36 @@
+//Selection Sort
+#include<stdio.h>
+int main(){
+	int a[20],i,j,temp,n,index,min;
+	printf("enter how many nos you want to input");
+	scanf("%d",&n);
+	printf("enter numbers");
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n-1;i++)
+	{
+		min=a[i];
+		index=i;
+		for(j=i+1;j<n;j++)
+		{
+			if(a[j]<min)
+			{
+				min=a[j];
+				index=j;
+			}
+		}
+		if(index!=i)
+		{
+			temp=a[i];
+			a[i]=a[index];
+			a[index]=temp;
+		}
+	
+	}
+		printf("values after Selection sort:");
+		for(i=0;i<n;i++)
+		{
+			printf("%d ",a[i]);
+		}
+}
